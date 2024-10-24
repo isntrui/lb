84914ce0b807da42be6fd660ac2bd8eeb46e5e3f
Artem Akopian <akopartem1@bk.ru>
2024-10-24 05:01:00 +0300

    fixes

--- src/main/java/ru/isntrui/lb/repositories/InviteRepository.kt
+++ src/main/java/ru/isntrui/lb/repositories/InviteRepository.kt
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import ru.isntrui.lb.models.Invite
 
@@ -15,10 +16,7 @@ interface InviteRepository : JpaRepository<Invite, Long> {
     @Modifying
     public fun deleteByCode(code: String)
 
-    @Transactional
     @Modifying
-    @Query("UPDATE Invite i SET i.isUsed = true WHERE i.code = :code and i.usedBy = :usedBy")
-    public fun setUsed(code: String, usedBy: Long)
-
-
+    @Query("UPDATE Invite i SET i.isUsed = true, i.usedBy.id = :userId WHERE i.code = :code")
+    fun setUsed(@Param("code") code: String, @Param("userId") userId: Long)
 }
--- src/main/java/ru/isntrui/lb/services/InviteService.kt
+++ src/main/java/ru/isntrui/lb/services/InviteService.kt
@@ -2,14 +2,31 @@ package ru.isntrui.lb.services
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import ru.isntrui.lb.models.Invite
 import ru.isntrui.lb.repositories.InviteRepository
 
 @Service
-class InviteService {
+open class InviteService {
+
     @Autowired
     private lateinit var inviteRepository: InviteRepository
 
-    public fun findByCode(code: String) : Invite? = inviteRepository.findByCode(code)
-    public fun use(code: String, userId: Long) = inviteRepository.setUsed(code, userId)
+    // Find an invite by its code
+    fun findByCode(code: String): Invite? {
+        return inviteRepository.findByCode(code)
+    }
+
+    // Set the invite as used by a specific user
+    @Transactional
+    open fun use(code: String, userId: Long) {
+        val invite = inviteRepository.findByCode(code) ?: throw IllegalArgumentException("Invalid invite code")
+
+        if (invite.isUsed) {
+            throw IllegalStateException("Invite has already been used")
+        }
+
+        // Set the invite as used and associate it with the userId
+        inviteRepository.setUsed(code, userId)
+    }
 }
