a9789906f4a5a67dd0a4792f3117b1b06f4fe0dd
Artem Akopian <akopartem1@bk.ru>
2024-10-24 22:24:53 +0300

    fixes

--- src/main/java/ru/isntrui/lb/models/Invite.kt
+++ src/main/java/ru/isntrui/lb/models/Invite.kt
@@ -9,20 +9,20 @@ open class Invite {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
+    open var id: Long? = null
 
     @Column(unique = true, nullable = false)
-    var email: String? = null
+    open var email: String? = null
 
     @Column(nullable = false)
-    var code: String = ""
+    open var code: String = ""
 
     @Column(name = "made_on", nullable = false)
-    var madeOn: LocalDate = LocalDate.now()
+    open var madeOn: LocalDate = LocalDate.now()
 
     @Column(nullable = false)
-    var isUsed: Boolean = false
+    open var isUsed: Boolean = false
 
     @ManyToOne(fetch = FetchType.LAZY)
-    var usedBy: User? = null
+    open var usedBy: User? = null
 }
