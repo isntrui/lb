c11cd65596919d9cfeb4afebbaf94573fa084d76
Artem Akopian <akopartem1@bk.ru>
2024-11-07 20:20:28 +0300

    fix

--- src/main/java/ru/isntrui/lb/models/Invite.kt
+++ src/main/java/ru/isntrui/lb/models/Invite.kt
@@ -7,28 +7,25 @@ import java.time.LocalDateTime
 
 @Entity
 @Table(name = "invites")
-open class Invite(emailC: String, codeC: String) {
+open class Invite(
+    @Column(unique = true, nullable = false)
+    @Email
+    open var email: String? = null,
+
+    @Column(nullable = false)
+    open var code: String = ""
+) {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     open var id: Long? = null
 
-    @Column(unique = true, nullable = false)
-    @Email
-    open var email: String? = null
-
-    @Column(nullable = false)
-    open var code: String = ""
-
     @Column(name = "made_on", nullable = false)
     @CreationTimestamp
-    open lateinit var madeOn: LocalDateTime;
+    open lateinit var madeOn: LocalDateTime
 
     @Column(nullable = false)
     open var isUsed: Boolean = false
 
-    init {
-        email = emailC
-        code = codeC
-    }
+    constructor() : this(null, "")
 }
